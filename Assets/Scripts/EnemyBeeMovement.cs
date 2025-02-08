773af8f4db3c0eb74aa8a41bffc445df59459fd4
uselessleaff <jianthelong@gmail.com>
2025-02-08 18:39:19 -0500

    the guys stay on their axis' now

--- Assets/Scripts/EnemyBeeMovement.cs
+++ Assets/Scripts/EnemyBeeMovement.cs
@@ -41,12 +41,12 @@ public class EnemyBeeMovement : MonoBehaviour
                 rb.linearVelocity = new Vector2(0, moveSpeed);
             }
 
-            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+            if (Mathf.Abs(transform.position.y - currentPoint.position.y) < 0.5f && currentPoint == pointB.transform)
             {
                 currentPoint = pointA.transform;
                 dirX *= -1;
             }
-            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+            if (Mathf.Abs(transform.position.y - currentPoint.position.y) < 0.5f && currentPoint == pointA.transform)
             {
                 currentPoint = pointB.transform;
                 dirX *= -1;
